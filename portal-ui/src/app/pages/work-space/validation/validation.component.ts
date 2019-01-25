246a71ca0d233e4edf333b78b7e5e1c57e1e3e36
Denis <fanaticFiz@outllok.com>
2019-01-25 16:54:38 +0300

    validation view

--- portal-ui/src/app/pages/work-space/validation/validation.component.ts
+++ portal-ui/src/app/pages/work-space/validation/validation.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
 import {NGXLogger} from "ngx-logger";
 import {Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material";
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../services/auth.service";
 import {LayersService} from "../../../services/geoserver/layers.service";
 import {NameHrefProjection} from "../../../services/geoserver/projections";
@@ -13,23 +13,53 @@ import {NameHrefProjection} from "../../../services/geoserver/projections";
 })
 export class ValidationComponent implements OnInit {
 
-  private layers: NameHrefProjection[];
+  private layers: NameHrefProjection[] = [
+    {
+      name: 'functionalZone',
+      href: 'http://',
+    },
+    {
+      name: 'Some other layer',
+      href: 'http://',
+    },
+    {
+      name: 'Some other layer 2',
+      href: 'http://',
+    },
+    {
+      name: 'Electrica',
+      href: 'http://',
+    }
+  ];
+
+  step = 0;
 
   constructor(private logger: NGXLogger,
               private router: Router,
               private snackBar: MatSnackBar,
               private authService: AuthService,
               private layersService: LayersService) {
-    this.authService.validateAuth();
+    // this.authService.validateAuth();
   }
 
   ngOnInit() {
     this.layersService.getAll()
         .subscribe((layers: NameHrefProjection[]) => {
-          this.layers = layers;
+          // this.layers = layers;
 
           this.logger.info('layersService.getAll: ', this.layers);
         });
   }
 
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
 }
