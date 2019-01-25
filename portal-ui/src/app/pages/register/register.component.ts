096f7a22406c524596f3bdd15c59df1436db55fd
a.kanivets <al.kanivets@mail.ru>
2019-01-25 17:06:15 +0300

    add validation minLength

--- portal-ui/src/app/pages/register/register.component.ts
+++ portal-ui/src/app/pages/register/register.component.ts
@@ -2,10 +2,8 @@ import {NGXLogger} from 'ngx-logger';
 import {Router} from '@angular/router';
 import {Component} from '@angular/core';
 import {MatSnackBar} from '@angular/material';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
-import { FormControl } from '@angular/forms';
-import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'crg-register',
@@ -14,34 +12,43 @@ import { ValidationErrors } from '@angular/forms';
 })
 export class RegisterComponent {
 
+
   registrationForm = this.fb.group({
     company: [null, Validators.required],
     contactPhone: [null, Validators.required],
     lastName: [null, Validators.required],
     firstName: [null, Validators.required],
-    email: [null, Validators.required],
-    password: [null, Validators.required, Validators.minLength(6)],
-    password_: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]],
+    passwords: this.fb.group({
+      password: [null, Validators.required],
+      password_: [null,[Validators.required]]
+    })
+    /*password: [null, Validators.required],
+    password_: [null,[Validators.required, this.passValidate]]*/
+
   });
+
   constructor(private fb: FormBuilder,
               private router: Router,
               private logger: NGXLogger,
               private snackBar: MatSnackBar,
-              private authService: AuthService) {}
+              private authService: AuthService) {
+  }
+
   onSubmit() {
     if (this.registrationForm.valid) {
       this.authService
-          .registration(this.registrationForm.getRawValue())
-          .subscribe(value => {
-            this.registrationForm.getRawValue();
+        .registration(this.registrationForm.getRawValue())
+        .subscribe(value => {
+          this.registrationForm.getRawValue();
 
-            this.snackBar.open('Регистрация прошла успешно', 'X', {duration: 5000});
-            this.router.navigateByUrl('/login');
-          }, errorResponse => {
-            Object.keys(errorResponse.error).forEach(value => {
-              this.snackBar.open(errorResponse.error[value][0], 'X', {duration: 5000});
-            });
+          this.snackBar.open('Регистрация прошла успешно', 'X', {duration: 5000});
+          this.router.navigateByUrl('/login');
+        }, errorResponse => {
+          Object.keys(errorResponse.error).forEach(value => {
+            this.snackBar.open(errorResponse.error[value][0], 'X', {duration: 5000});
           });
+        });
     } else {
       alert('Not valid!');
     }
