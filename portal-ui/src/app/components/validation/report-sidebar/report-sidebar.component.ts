3ff332c58ad94c11364663b45137e496a6786c83
a.kanivets <al.kanivets@mail.ru>
2019-02-25 16:18:33 +0300

    fix body scrollbar add scrollbar to edit-object

--- portal-ui/src/app/components/validation/report-sidebar/report-sidebar.component.ts
+++ portal-ui/src/app/components/validation/report-sidebar/report-sidebar.component.ts
@@ -28,7 +28,7 @@ export class ReportSidebarComponent implements OnInit, OnDestroy {
   step = 0;
   isValidationInited = false;
 
-  isEditMode = false;
+  isEditMode = true;
   objectsToEdit: ObjectDto[] = [];
 
   private unsubscribe$: Subject<void> = new Subject<void>();
