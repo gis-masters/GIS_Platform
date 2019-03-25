600b16c552a78b7e7e55cd86f2aee9244d3d2c80
Denis <fanaticFiz@outlook.com>
2019-03-25 18:05:59 +0300

    split featureTypes by geometry for select then import

--- portal-ui/src/app/components/mapping-card/mapping-card.component.ts
+++ portal-ui/src/app/components/mapping-card/mapping-card.component.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import {NGXLogger} from 'ngx-logger';
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {
@@ -31,7 +32,11 @@ export class MappingCardComponent implements OnInit, OnChanges {
 
     this.ruleService.getRules()
         .subscribe((entityTypesDefinition: FeatureXsdDefinition) => {
-          this.entityTypes = entityTypesDefinition.xsdFeatures;
+          this.logger.info('MappingCardComponent this.entityTypes: ', entityTypesDefinition.xsdFeatures);
+
+          this.entityTypes = this.handleFeatures(entityTypesDefinition.xsdFeatures);
+          this.logger.info('MappingCardComponent this.entityTypes: ', this.entityTypes);
+          // this.entityTypes = entityTypesDefinition.xsdFeatures;
         });
   }
 
@@ -45,7 +50,7 @@ export class MappingCardComponent implements OnInit, OnChanges {
   }
 
   entityTypeSelected(selected: string) {
-    const xsdFeature = this.entityTypes.find((type: XsdFeature) => type.name === selected);
+    const xsdFeature = this.entityTypes.find((type: XsdFeature) => type.tableName === selected);
 
     this.importService.importFlow.setTable(this.layer.originalName, xsdFeature.tableName);
     this.typeProperties = [];
@@ -57,4 +62,61 @@ export class MappingCardComponent implements OnInit, OnChanges {
     });
   }
 
+  private handleFeatures(xsdFeatures: XsdFeature[]) {
+    const result: XsdFeature[] = [];
+
+    xsdFeatures.forEach((xsdFeature: XsdFeature) => {
+      const property = this.getGeometryProperty(xsdFeature);
+
+      if (!!property && property.allowedValues.length > 0) {
+        property.allowedValues.forEach(geometryType => {
+          const copyOfFeature: XsdFeature = _.cloneDeep(xsdFeature);
+          switch (geometryType) {
+            case 'Curve':
+              // Do nothing
+              break;
+            case 'Polygon':
+              this.handleFeature(copyOfFeature, property, 'Polygon');
+
+              result.push(copyOfFeature);
+              break;
+            case 'Point':
+              this.handleFeature(copyOfFeature, property, 'Point');
+
+              result.push(copyOfFeature);
+              break;
+            case 'LineString':
+              this.handleFeature(copyOfFeature, property, 'LineString');
+
+              result.push(copyOfFeature);
+              break;
+            default:
+              this.logger.info('Unsupported geometry type: ', geometryType, copyOfFeature.tableName);
+          }
+        });
+      } else {
+        this.logger.warn('Not exist geometry in feature?', xsdFeature.name, property);
+      }
+    });
+
+    return result;
+  }
+
+  private getGeometryProperty(xsdFeature: XsdFeature): SimpleProperty {
+    return _.find(xsdFeature.properties, (property: SimpleProperty) => property.valueType === 'GEOMETRY');
+  }
+
+  // Заменить у фичи свойство геометрии с одним типом, поправить название фичи
+  private handleFeature(copyOfFeature: XsdFeature, property: SimpleProperty, gType: string) {
+    const propertyCopy: SimpleProperty = _.cloneDeep(property);
+    propertyCopy.allowedValues = [gType];
+
+    _.remove(copyOfFeature.properties, (prop: SimpleProperty) => prop.valueType === 'GEOMETRY');
+
+    if (gType !== 'Polygon' && gType !== 'LineString') {
+      copyOfFeature.tableName = copyOfFeature.tableName + '_' + gType.toLowerCase();
+    }
+
+    copyOfFeature.properties.push(propertyCopy);
+  }
 }
