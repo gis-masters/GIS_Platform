02c03a09a523c8d6ac0b8c89cd9acadc68277ed4
Denis <fanaticFiz@outlook.com>
2019-03-29 16:32:27 +0300

    test

--- portal-ui/src/environments/environment.ts
+++ portal-ui/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   version: 'Default 0.0.9',
   server: {
-    'host': 'localhost',
+    'host': '10.10.10.148',
     'port': 8100
   },
   scratchWorkspaceName: 'scratch_workspace'
