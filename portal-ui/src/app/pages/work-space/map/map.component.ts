f3e4286c918901033c5c0a428551dc1ef2531463
Denis <fanaticFiz@outlook.com>
2019-08-18 06:08:54 +0300

    back old way "position to layer"

--- portal-ui/src/app/pages/work-space/map/map.component.ts
+++ portal-ui/src/app/pages/work-space/map/map.component.ts
@@ -1,7 +1,5 @@
-import {fromLonLat} from 'ol/proj';
 import {NGXLogger} from 'ngx-logger';
 import {Subject, throwError} from 'rxjs';
-import Projection from 'ol/proj/Projection';
 import {MatSnackBar} from '@angular/material';
 import {MediaMatcher} from '@angular/cdk/layout';
 import {WfsUtil} from '../../../services/open-layer/WfsUtil';
@@ -241,17 +239,13 @@ export class MapComponent implements OnInit, OnDestroy {
 
           // Позиционируемся на первом из загруженных слоев
           if (layers.length > 0) {
-            this.featureTypesService.getByName(layers[0])
+            this.wfsService.getFeatures(layers[0].complexName)
                 .pipe(takeUntil(this.unsubscribe$))
-                .subscribe((response: FeatureType) => {
-                  if (response) {
-                    const bbox = response.latLonBoundingBox;
-                    const min = fromLonLat([bbox.minx, bbox.miny], new Projection({code: 'EPSG:3857'}));
-                    const max = fromLonLat([bbox.maxx, bbox.maxy], new Projection({code: 'EPSG:3857'}));
-
-                    this.openLayers.fitToBbox([min[0], min[1], max[0], max[1]], [50, 50, 50, 50]);
+                .subscribe((fCollection: WfsFeatureCollection) => {
+                  if (fCollection && fCollection.bbox) {
+                    this.openLayers.fitToBbox(fCollection.bbox, [50, 50, 50, 50]);
                   } else {
-                    this.logger.info('Cant position to layer', response);
+                    this.logger.info('Cant position to layer', fCollection);
                   }
                 });
           }
