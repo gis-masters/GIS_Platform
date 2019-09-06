ce5c8fecff76612ba255b6f54bcdfea69053108e
Denis <fanaticFiz@outlook.com>
2019-09-07 00:29:33 +0300

    getting specific legend

--- portal-ui/src/app/services/geoserver/wms.service.ts
+++ portal-ui/src/app/services/geoserver/wms.service.ts
@@ -3,7 +3,7 @@ import {NGXLogger} from 'ngx-logger';
 import {Injectable} from '@angular/core';
 import {catchError} from 'rxjs/operators';
 import {BaseService} from '../base.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {ServerPropertiesService} from '../server-properties.service';
 
 @Injectable({
@@ -12,9 +12,13 @@ import {ServerPropertiesService} from '../server-properties.service';
 export class WmsService {
 
   private _baseUrl = this.serverProp.geoServerUrl + '/wms';
-  private default_request = 'GetLegendGraphic';
-  private default_version = '1.0.0';
-  private default_format = 'image/png';
+  private request = 'GetLegendGraphic';
+  private version = '1.3.0';
+  private format = 'image/png';
+  private width = '40';
+  private height = '20';
+  private legendOptions = 'fontAntiAliasing:true';
+
   constructor(private http: HttpClient,
               private logger: NGXLogger,
               private baseService: BaseService,
@@ -23,15 +27,42 @@ export class WmsService {
   }
 
   // http://localhost:8080/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=topp:states
-  getLinkForLegend(layerName: string): string {
-    return this._baseUrl + '?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=' + layerName;
-  }
-
-  getLegend(complexLayerName: string): Observable<any> {
-    // this.logger.info('Try get legend for: ', complexLayerName);
+  getFullLegend(complexLayerName: string): Observable<any> {
+    const params = new HttpParams()
+      .set('REQUEST', this.request)
+      .set('VERSION', this.version)
+      .set('FORMAT', this.format)
+      .set('WIDTH', this.width)
+      .set('HEIGHT', this.height)
+      .set('LAYER', complexLayerName);
 
     return this.http
-               .get(this.getLinkForLegend(complexLayerName), {responseType: 'blob'})
+               .get(this._baseUrl, {responseType: 'blob', params: params})
+               .pipe(
+                 catchError(this.baseService.handleError('getLegend', []))
+               );
+  }
+
+  /**
+   * Get a graphic that is representative of specific rule by their name.
+   *
+   * @param complexLayerName  Название слоя в формате 'workspace:layerName'
+   * @param ruleName          Название правила в стиле. Ожидаем что стили будут основаны на атрибуте classid,
+   *                          и будут содержать его значение в названии.
+   */
+  getLegendRule(complexLayerName: string, ruleName: string): Observable<any> {
+    const params = new HttpParams()
+      .set('REQUEST', this.request)
+      .set('VERSION', this.version)
+      .set('FORMAT', this.format)
+      .set('WIDTH', this.width)
+      .set('HEIGHT', this.height)
+      .set('LEGEND_OPTIONS', this.legendOptions)
+      .set('LAYER', complexLayerName)
+      .set('RULE', ruleName);
+
+    return this.http
+               .get(this._baseUrl, {responseType: 'blob', params: params})
                .pipe(
                  catchError(this.baseService.handleError('getLegend', []))
                );
