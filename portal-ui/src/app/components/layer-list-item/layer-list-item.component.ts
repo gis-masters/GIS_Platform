d5de376e8af41f7e69afae08e15e7996c9b0857b
zakaratcha <zakaratcha@ya.ru>
2019-09-11 22:57:59 +0300

    fix layers dragging & refactoring

--- portal-ui/src/app/components/layer-list-item/layer-list-item.component.ts
+++ portal-ui/src/app/components/layer-list-item/layer-list-item.component.ts
@@ -1,24 +1,40 @@
-import {NGXLogger} from 'ngx-logger';
-import {Component, Input, OnInit} from '@angular/core';
-import {WmsService} from '../../services/geoserver/wms.service';
+import {Component, Input, OnInit, ViewChild, OnDestroy, Output, EventEmitter} from '@angular/core';
+import {Subject} from 'rxjs';
+import {filter, takeUntil} from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
+import { MatMenuTrigger } from '@angular/material/menu';
+
+import {ActionType, SideBarManager, SidebarType} from '../../services/side-bar-manager.service';
 import {CrgLayer} from '../../services/geoserver/layers.service';
+import {ExportService} from '../../services/crg/export.service';
+import {WmsService} from '../../services/geoserver/wms.service';
 import {StringUtil} from '../../services/util/StringUtil';
+import {ConfirmDialogComponent, ConfirmDialogData} from '../dialogs/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'crg-layer-list-item',
   templateUrl: './layer-list-item.component.html',
-  styleUrls: ['./layer-list-item.component.css']
+  styleUrls: ['./layer-list-item.component.scss']
 })
-export class LayerListItemComponent implements OnInit {
+export class LayerListItemComponent implements OnInit, OnDestroy {
 
   @Input() layer: CrgLayer;
+  @Output() deleteLayer = new EventEmitter<CrgLayer>();
+
+  @ViewChild(MatMenuTrigger, { static: false })
+  contextMenu: MatMenuTrigger;
+
+  contextMenuPosition: {x: string, y: string} = { x: '0px', y: '0px' };
 
   imageToShow: any;
   isImageLoaded = false;
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
+
   constructor(private wmsService: WmsService,
-              private logger: NGXLogger) {
-  }
+              private exportService: ExportService,
+              private sideBarManager: SideBarManager,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.wmsService
@@ -26,6 +42,54 @@ export class LayerListItemComponent implements OnInit {
         .subscribe(data => {
           this.createImageFromBlob(data);
         });
+  }
+
+  onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+    this.contextMenuPosition.x = event.clientX + 'px';
+    this.contextMenuPosition.y = event.clientY + 'px';
+    this.contextMenu.openMenu();
+  }
+
+  openAttributeTable() {
+    this.sideBarManager.do({
+      target: SidebarType.ATTRIBUTES,
+      action: ActionType.OPEN,
+      data: this.layer
+    });
+  }
+
+  export() {
+    this.exportService
+        .export({format: 'ESRI Shapefile', layers: [this.layer.name]})
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(() => {
+          // TODO: Ответ пойдет по вебсокету, но здесь его нужно подстраховать
+          this.sideBarManager.do({target: SidebarType.INFO, action: ActionType.OPEN});
+        });
+  }
+
+  delete() {
+    const dialogData: ConfirmDialogData = {
+      title: 'Удалить слой: ' + this.layer.title,
+      approveBtnName: 'Удалить'
+    };
+
+    this.dialog
+        .open(ConfirmDialogComponent, {width: '400px', data: dialogData})
+        .afterClosed().pipe(filter(value => !!value))
+        .subscribe(() => {
+          this.deleteLayer.emit(this.layer);
+        });
+  }
+
+  getGeometryType(name: string) {
+    return StringUtil.splitGeomType(name);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   private createImageFromBlob(image: Blob) {
@@ -38,9 +102,5 @@ export class LayerListItemComponent implements OnInit {
     if (image) {
       reader.readAsDataURL(image);
     }
-  }
-
-  getGeometryType(name: string) {
-    return StringUtil.splitGeomType(name);
   }
 }
--- portal-ui/src/app/components/layers-sidebar/layers-sidebar.component.ts
+++ portal-ui/src/app/components/layers-sidebar/layers-sidebar.component.ts
@@ -1,54 +1,26 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Subject} from 'rxjs';
 import {NGXLogger} from 'ngx-logger';
 import {CrgLayer} from '../../services/geoserver/layers.service';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
-import { MatMenuTrigger } from '@angular/material/menu';
 import {OpenLayersService} from '../../services/open-layer/open-layers.service';
 import {ActionType, SideBarManager, SidebarType} from '../../services/side-bar-manager.service';
-import {ExportService} from '../../services/crg/export.service';
-import {Process} from '../../services/crg/models';
-import {CommunicationService} from '../../services/communication.service';
-import {filter, takeUntil} from 'rxjs/operators';
-import {MatDialog} from '@angular/material/dialog';
-import {ConfirmDialogComponent, ConfirmDialogData} from '../dialogs/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'crg-layers-sidebar',
   templateUrl: './layers-sidebar.component.html',
-  styleUrls: ['./layers-sidebar.component.css']
+  styleUrls: ['./layers-sidebar.component.scss']
 })
-export class LayersSidebarComponent implements OnInit, OnDestroy {
+export class LayersSidebarComponent {
 
-  @Input() isActive;
+  @Input() isActive: boolean;
   @Input() layers: CrgLayer[];
   @Output() deleteLayer = new EventEmitter<CrgLayer>();
 
-  @ViewChild(MatMenuTrigger, { static: false })
-  contextMenu: MatMenuTrigger;
-  contextMenuPosition = { x: '0px', y: '0px' };
-
-  private unsubscribe$: Subject<void> = new Subject<void>();
-  private selectedLayer: CrgLayer;
-
-  constructor(private logger: NGXLogger,
-              private exportService: ExportService,
-              private openLayers: OpenLayersService,
-              private dialog: MatDialog,
-              private communicationService: CommunicationService,
-              private sideBarManager: SideBarManager) {
-  }
-
-  ngOnInit() {}
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+  constructor(private openLayers: OpenLayersService) { }
 
   drop(event: CdkDragDrop<string[]>) {
-    console.log('drop(event: CdkDragDrop<string[]>)', event.previousIndex, event.currentIndex);
     moveItemInArray(this.layers, event.previousIndex, event.currentIndex);
 
     this.layers.forEach((layer, index) => {
@@ -65,50 +37,7 @@ export class LayersSidebarComponent implements OnInit, OnDestroy {
     this.openLayers.changeLayersVisibility(nameOfSelectedLayers);
   }
 
-  onContextMenu(event: MouseEvent, layer: CrgLayer) {
-    event.preventDefault();
-
-    this.selectedLayer = layer;
-    this.contextMenuPosition.x = event.clientX + 'px';
-    this.contextMenuPosition.y = event.clientY + 'px';
-    this.contextMenu.openMenu();
-  }
-
-  openAttributeTable() {
-    this.sideBarManager.do({target: SidebarType.ATTRIBUTES, action: ActionType.OPEN, data: this.selectedLayer});
-  }
-
-  export() {
-    if (this.selectedLayer) {
-      this.exportService
-          .export({format: 'ESRI Shapefile', layers: [this.selectedLayer.name]})
-          .pipe(takeUntil(this.unsubscribe$))
-          .subscribe((process: Process) => {
-            // TODO: Ответ пойдет по вебсокету, но здесь его нужно подстраховать
-            this.logger.info('export shape:', process);
-
-            this.sideBarManager.do({target: SidebarType.INFO, action: ActionType.OPEN});
-          });
-    } else {
-      this.logger.warn('Empty selected layer: ', this.selectedLayer);
-    }
-  }
-
-  delete() {
-    if (this.selectedLayer) {
-      const dialogData: ConfirmDialogData = {
-        title: 'Удалить слой: ' + this.selectedLayer.title,
-        approveBtnName: 'Удалить'
-      };
-
-      this.dialog
-          .open(ConfirmDialogComponent, {width: '400px', data: dialogData})
-          .afterClosed().pipe(filter(value => !!value))
-          .subscribe(() => {
-            this.deleteLayer.emit(this.selectedLayer);
-          });
-    } else {
-      this.logger.warn('Empty selected layer: ', this.selectedLayer);
-    }
+  onDeleteLayer (layer: CrgLayer) {
+    this.deleteLayer.emit(layer);
   }
 }
