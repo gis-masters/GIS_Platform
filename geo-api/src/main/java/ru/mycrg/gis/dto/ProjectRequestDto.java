c7ff103ca73edb8fa56669505ced95682fdec402
Denis <fanaticFiz@outlook.com>
2019-09-12 11:23:09 +0300

    fix regular expression: allow whitespace

--- geo-api/src/main/java/ru/mycrg/gis/dto/ProjectRequestDto.java
+++ geo-api/src/main/java/ru/mycrg/gis/dto/ProjectRequestDto.java
@@ -8,7 +8,7 @@ public class ProjectRequestDto extends BaseRequest {
 
     @NotBlank(message = "Забыли указать название проекта")
     @Size(min=3, max=50, message = "Не менее 3 и не более 50 символов")
-    @Pattern(regexp = "^([a-zA-Zа-яА-ЯёЁ]{1}[a-zA-Zа-яА-ЯёЁ0-9._-]+)$",
+    @Pattern(regexp = "^([a-zA-Zа-яА-ЯёЁ]{1}[a-zA-Zа-яА-ЯёЁ0-9._ -]+)$",
             message = "Должно начинаться с буквы. Затем может содержать: буквы, цифры и символы .-_")
     private String projectName;
 
