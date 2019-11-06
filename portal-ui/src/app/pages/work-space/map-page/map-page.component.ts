e78e6d33b4b2466ec7602c9213dedac336669493
zakaratcha <zakaratcha@ya.ru>
2019-11-06 14:15:15 +0000

    Merged PR 105: feat(ui:map) add project name on map page header
    feat(ui:map) add project name on map page header
    
    Related work items: #299

--- portal-ui/src/app/pages/work-space/map-page/map-page.component.ts
+++ portal-ui/src/app/pages/work-space/map-page/map-page.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProjectsService } from '../../../services/crg/projects.service';
 
 @Component({
   selector: 'crg-map-page',
   templateUrl: './map-page.component.html'
 })
-export class MapPageComponent { }
+export class MapPageComponent implements OnInit {
+  projectName: string;
+
+  constructor (private projectsService: ProjectsService) { }
+
+  async ngOnInit() {
+    const project = await this.projectsService.getCurrent();
+    this.projectName = project.crgProject.internalName;
+  }
+}
