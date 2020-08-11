1a1a71d579ba7dde3beb22c2686ad8a743c026d7
Алексеев Денис <fanaticFiz@outlook.com>
2020-08-11 09:35:51 +0000

    Merged PR 297: fix(auth): fix nullpointer exc
    Related work items: #1626

--- auth-service/src/main/java/ru/mycrg/auth_service/security/CustomTokenConverter.java
+++ auth-service/src/main/java/ru/mycrg/auth_service/security/CustomTokenConverter.java
@@ -1,5 +1,7 @@
 package ru.mycrg.auth_service.security;
 
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -8,6 +10,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.transaction.annotation.Transactional;
 import ru.mycrg.auth_service.dto.IdNameProjection;
 import ru.mycrg.auth_service.entity.User;
+import ru.mycrg.auth_service.exeptions.AuthServiceException;
 import ru.mycrg.auth_service.repository.UserRepository;
 
 import java.util.HashMap;
@@ -16,6 +19,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Log4j2
 public class CustomTokenConverter extends JwtAccessTokenConverter {
 
     @Autowired
@@ -32,35 +36,43 @@ public class CustomTokenConverter extends JwtAccessTokenConverter {
     @Override
     @Transactional
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-        if (authentication.getOAuth2Request().getGrantType().equalsIgnoreCase("password")) {
-            String userName = authentication.getName();
+        try {
+            // Add to token additional info
+            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(collectAdditionalInfo(authentication.getName()));
 
-            Optional<User> byUsername = userRepository.findByUsername(userName);
+            accessToken = super.enhance(accessToken, authentication);
 
-            final Map<String, Object> additionalInfo = new HashMap<>();
-            if (byUsername.isPresent()) {
-                User user = byUsername.get();
+            // But return token without additionalInformation
+            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(new HashMap<>());
 
-                List<IdNameProjection> usersOrganizations = user.getOrganizations().stream()
-                        .map(org -> new IdNameProjection(org.getId(), org.getName()))
-                        .collect(Collectors.toList());
+             return accessToken;
+        } catch (Exception e) {
+            throw new AuthServiceException("Token converter error");
+        }
+    }
 
-                List<IdNameProjection> usersGroups = user.getGroups().stream()
-                        .map(group -> new IdNameProjection(group.getId(), group.getName()))
-                        .collect(Collectors.toList());
+    @NotNull
+    private Map<String, Object> collectAdditionalInfo(String userName) {
+        final Map<String, Object> additionalInfo = new HashMap<>();
 
-                additionalInfo.put("user_id", user.getId());
-                additionalInfo.put("groups", usersGroups);
-                additionalInfo.put("organizations", usersOrganizations);
-            }
+        Optional<User> byUsername = userRepository.findByUsername(userName);
 
-            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+        if (byUsername.isPresent()) {
+            User user = byUsername.get();
+
+            List<IdNameProjection> usersOrganizations = user.getOrganizations().stream()
+                    .map(org -> new IdNameProjection(org.getId(), org.getName()))
+                    .collect(Collectors.toList());
+
+            List<IdNameProjection> usersGroups = user.getGroups().stream()
+                    .map(group -> new IdNameProjection(group.getId(), group.getName()))
+                    .collect(Collectors.toList());
+
+            additionalInfo.put("user_id", user.getId());
+            additionalInfo.put("groups", usersGroups);
+            additionalInfo.put("organizations", usersOrganizations);
         }
 
-        accessToken = super.enhance(accessToken, authentication);
-
-        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(new HashMap<>());
-
-        return accessToken;
+        return additionalInfo;
     }
 }
