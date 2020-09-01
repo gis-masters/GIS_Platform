0d6dab54cb98dd2c289bcadef878294c0195dee6
Алексеев Денис <fanaticFiz@outlook.com>
2020-09-01 09:23:17 +0000

    Merged PR 309: fix(api-service): fix schemas migration
    fix(api-service): fix schemas migration
    
    Related work items: #1703

--- geo-api/src/main/resources/db/migration/V13__joinSchemas.sql
+++ geo-api/src/main/resources/db/migration/V13__joinSchemas.sql
@@ -1,3 +1,5 @@
+SELECT setval('public.xsd_rules_id_seq', 1000, true);
+
 ALTER TABLE public.xsd_rules
     ADD COLUMN custom_rule text;
 
@@ -9,3 +11,5 @@ ALTER TABLE public.xsd_rules
 
 INSERT INTO public.xsd_rules(name, class_rule, custom_rule, calculated_fields)
   select xr.name, xr.class_rule, cr.class_rule, cr.calculated_fields from public.xsd_rules as xr JOIN public.custom_rules as cr ON xr.name = cr.class_name;
+
+DELETE FROM public.xsd_rules WHERE id < 1000;
