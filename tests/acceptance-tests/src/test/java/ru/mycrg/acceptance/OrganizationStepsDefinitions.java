8a62b377176b4df2028e7c661196e0519cfd60df
Сетков Фёдор <setfed@outlook.com>
2020-09-08 09:14:45 +0000

    Merged PR 319: createValidOrg + createInvalidOrg
    createValidOrg + createInvalidOrg
    
    Related work items: #1670, #1743

--- tests/acceptance-tests/src/test/java/ru/mycrg/acceptance/OrganizationStepsDefinitions.java
+++ tests/acceptance-tests/src/test/java/ru/mycrg/acceptance/OrganizationStepsDefinitions.java
@@ -2,19 +2,27 @@ package ru.mycrg.acceptance;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.messages.internal.com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import ru.mycrg.auth_service_contract.dto.OrganizationCreateDto;
 import ru.mycrg.auth_service_contract.dto.UserCreateDto;
 
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class OrganizationStepsDefinitions {
 
@@ -25,6 +33,10 @@ public class OrganizationStepsDefinitions {
 
     private static RequestSpecification request;
     private static Response response;
+
+    private static String orgId;
+    private static Cookie cookie;
+    private static Duration authRequestDurationRestriction = Duration.ofSeconds(60);
 
     @Before
     public static void setup() {
@@ -40,7 +52,7 @@ public class OrganizationStepsDefinitions {
         request = RestAssured.given();
         request.baseUri(testServerHost);
         request.port(testServerPort);
-        request.basePath("/organizations");
+        request.basePath("/");
     }
 
     @When("Пользователь вводит корректные данные")
@@ -53,15 +65,114 @@ public class OrganizationStepsDefinitions {
         String payload = new Gson().toJson(org);
 
         response = request
-                    .body(payload)
-                    .contentType(ContentType.JSON)
+                .body(payload)
+                .contentType(ContentType.JSON)
                 .when()
-                    .post("/init");
+                .post("/organizations/init");
     }
 
-    @Then("Сервер отвечает статус-кодом {int}")
+    @Then("Сервер отвечает со статус-кодом {int}")
     public void assertResponseCode(int status) {
         assertEquals(status, response.getStatusCode());
     }
 
+    @And("в заголовке Location передает ID созданной организации")
+    public void extractLocation() {
+        String header = response.getHeader("Location");
+        Pattern pattern = Pattern.compile("\\d+$");
+        Matcher matcher = pattern.matcher(header);
+
+        while (matcher.find()) {
+            orgId = matcher.group();
+        }
+
+        assertNotNull(orgId);
+    }
+
+    @When("Пользователь пытается авторизоваться")
+    public void tryToGetAuthorized(DataTable dataTable) throws InterruptedException {
+        setup();
+
+        List<String> data = dataTable.asList();
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("username", data.get(0));
+        queryParams.put("password", data.get(1));
+        queryParams.put("grant_type", data.get(2));
+
+        do {
+            int waitDelayInMillis = 5000;
+            response = request
+                    .formParams(queryParams)
+                    .when()
+                    .post("/oauth/token");
+
+            Thread.sleep(waitDelayInMillis);
+            authRequestDurationRestriction = authRequestDurationRestriction.minusMillis(waitDelayInMillis);
+
+        } while (response.getStatusCode() == 401 && authRequestDurationRestriction.getSeconds() > 0);
+    }
+
+    @Then("Сервер авторизует пользователя")
+    public void serverAuthorizeUser() {
+        cookie = response.getDetailedCookie("crgAuthCookie");
+        assertNotNull(cookie);
+    }
+
+    @When("Пользователь проверяет создана ли организация")
+    public void getOrgInfo() {
+        response = request.basePath("/organizations/")
+                .cookie(cookie)
+                .get(orgId);
+    }
+
+    @And("Сервер отвечает с полем status = PROVISIONED")
+    public void checkIsOrgProvisioned() {
+        JsonPath jsonPath = response.jsonPath();
+        String status = jsonPath.get("status");
+
+        assertTrue(status.equalsIgnoreCase("PROVISIONED"));
+    }
+
+    @When("Пользователь вводит некорректные данные")
+    public void createInvalidOrganization(DataTable dataTable) {
+        List<String> data = dataTable.asList();
+        UserCreateDto owner = new UserCreateDto(replaceString(data.get(2)), replaceString(data.get(3)),
+                replaceString(data.get(4)), replaceString(data.get(5)));
+        OrganizationCreateDto org = new OrganizationCreateDto(replaceString(data.get(0)), replaceString(data.get(1)),
+                owner);
+
+        String payload = new Gson().toJson(org);
+
+        response = request
+                .body(payload)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/organizations/init");
+    }
+
+    private String replaceString(String input) {
+        if (input == null) return "";
+
+        switch (input) {
+            case "LONG_ORG_NAME":
+                return "testOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgte" +
+                        "stOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtes" +
+                        "tOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtest" +
+                        "OrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestO" +
+                        "rgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOrgtestOr" +
+                        "gtestOrgtestOrgtestOrgtestOrgtestOrgtestOrg";
+            case "LONG_ORG_PHONE":
+                return "123456789012345678901";
+            case "LONG_ADMIN_NAME":
+                return "testNametestNametestNametestNametestNametestNametestName";
+            case "LONG_ADMIN_SURNAME":
+                return "testSurnametestSurnametestSurnametestSurnametestSurnametestSurnametestSurnametestSurnametestS" +
+                        "urnametestSurname";
+            case "LONG_ADMIN_EMAIL":
+                return "emailemailemailemailemailemailemailemailemailemailemail@email.com";
+            default:
+                return input;
+        }
+    }
 }
