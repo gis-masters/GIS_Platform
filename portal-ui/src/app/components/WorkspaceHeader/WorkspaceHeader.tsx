024d21a543ebb3c4c53bb2f0860730cad2d597bb
Александр Кривохижин <zakaratcha@ya.ru>
2020-12-11 21:53:30 +0000

    Merged PR 408: fix(ui:favicon) fix favicon for workspace pages
    Related work items: #2173

--- portal-ui/src/app/components/WorkspaceHeader/WorkspaceHeader.tsx
+++ portal-ui/src/app/components/WorkspaceHeader/WorkspaceHeader.tsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { cn } from '@bem-react/classname';
+
+import { Favicon } from '../Favicon/Favicon';
 
 import { WorkspaceHeaderBurger } from './Burger/WorkspaceHeader-Burger';
 import { WorkspaceHeaderOrganisation } from './Organisation/WorkspaceHeader-Organisation';
@@ -11,16 +13,13 @@ import '!style-loader!css-loader!sass-loader!./WorkspaceHeader.scss';
 
 const cnWorkspaceHeader = cn('WorkspaceHeader');
 
-export class WorkspaceHeader extends Component {
-  render() {
-    return (
-      <div className={cnWorkspaceHeader()}>
-        <WorkspaceHeaderBurger />
-        <WorkspaceHeaderOrganisation />
-        <WorkspaceHeaderBreadcrumbs />
-        <WorkspaceHeaderButtons />
-        <WorkspaceHeaderLoader />
-      </div>
-    );
-  }
-}
+export const WorkspaceHeader: FC = () => (
+  <div className={cnWorkspaceHeader()}>
+    <Favicon />
+    <WorkspaceHeaderBurger />
+    <WorkspaceHeaderOrganisation />
+    <WorkspaceHeaderBreadcrumbs />
+    <WorkspaceHeaderButtons />
+    <WorkspaceHeaderLoader />
+  </div>
+);
