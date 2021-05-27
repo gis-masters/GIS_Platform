cb7522f63b2fec2e608dbe9d5b6d0786b6ef2c6f
Александр Кривохижин <zakaratcha@ya.ru>
2021-05-27 15:55:27 +0000

    Merged PR 557: fix(ui:map) raise ScaleLine halo and remove comma
    Related work items: #2963, #2983

--- portal-ui/src/app/services/ol/ScaleLine.ts
+++ portal-ui/src/app/services/ol/ScaleLine.ts
@@ -12,4 +12,9 @@ export class ScaleLine extends BaseScaleLine {
   createStepText(i: number, width: number, isLast: boolean, scale: number, suffix: string): string {
     return super.createStepText(i, width, isLast, scale, normalUnits[suffix] || suffix);
   }
+
+  createScaleBar(width: number, scale: number, suffix: string): number {
+    // @ts-ignore
+    return super.createScaleBar(width, scale, suffix).replace(/,(\d\d\d)/g, ' $1');
+  }
 }
