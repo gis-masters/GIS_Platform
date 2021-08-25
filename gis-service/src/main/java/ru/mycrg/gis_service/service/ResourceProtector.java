d4ed97c94484ecd53a5ca9ae09b9f2950ba39a6a
Алексеев Денис <fanaticFiz@outlook.com>
2021-08-25 13:59:33 +0000

    Merged PR 651: fix(gis-service): root and org_admin is absolute owner
    Related work items: #3367, #3368

--- gis-service/src/main/java/ru/mycrg/gis_service/service/ResourceProtector.java
+++ gis-service/src/main/java/ru/mycrg/gis_service/service/ResourceProtector.java
@@ -31,14 +31,14 @@ public class ResourceProtector {
 
         AtomicReference<String> bestRole = new AtomicReference<>();
         permissions.stream()
-               .filter(permission -> userIds.contains(permission.getPrincipalId()))
-               .forEach(permission -> {
-                   if (permission.getRole().equals(OWNER.name())) {
-                       bestRole.set(OWNER.name());
-                   } else if (permission.getRole().equals(VIEWER.name())) {
-                       bestRole.set(VIEWER.name());
-                   }
-               });
+                   .filter(permission -> userIds.contains(permission.getPrincipalId()))
+                   .forEach(permission -> {
+                       if (permission.getRole().equals(OWNER.name())) {
+                           bestRole.set(OWNER.name());
+                       } else if (permission.getRole().equals(VIEWER.name())) {
+                           bestRole.set(VIEWER.name());
+                       }
+                   });
 
         if (bestRole.get() == null) {
             return Optional.empty();
@@ -47,13 +47,23 @@ public class ResourceProtector {
         }
     }
 
+    /**
+     * Считаем что пользователь является владельцем ресурса, если:
+     * <li> Пользователь имеет OWNER право.
+     * <li> Пользователей является GLOBAL_ADMIN или ORG_ADMIN.
+     */
     public boolean isOwner(Project project) {
-        final Optional<String> oRole = defineBestRole(project.getPermissions());
+        return authenticationFacade.isOrganizationAdmin()
+                || isUserHasOwnPermission(project)
+                || authenticationFacade.isRoot();
+    }
 
+    private boolean isUserHasOwnPermission(Project project) {
+        final Optional<String> oRole = defineBestRole(project.getPermissions());
         if (oRole.isEmpty()) {
             return false;
-        } else {
-            return Objects.equals(oRole.get(), OWNER.name());
         }
+
+        return Objects.equals(OWNER.name(), oRole.get());
     }
 }
