e7ae945953bd1ba2319213d61f12f1c1af557cbe
Дмитрий Хребтов <framzik@gmail.com>
2021-09-09 15:54:34 +0000

    Merged PR 669: delete unused db
    Related work items: #3427

--- data-service/src/main/java/ru/mycrg/data_service/dao/CrgMigrationHandler.java
+++ data-service/src/main/java/ru/mycrg/data_service/dao/CrgMigrationHandler.java
@@ -56,6 +56,7 @@ public class CrgMigrationHandler {
                 ScriptUtils.executeSqlScript(connection, ctx.getResource("classpath:sql/M8__addSrid314and315.sql"));
                 ScriptUtils.executeSqlScript(connection,
                                              ctx.getResource("classpath:sql/M9__updateSchemasAndTables.sql"));
+                ScriptUtils.executeSqlScript(connection, ctx.getResource("classpath:sql/M10__delete_unused_tables.sql"));
             }
 
             tempDataSource.close();
---
+++ data-service/src/main/resources/sql/M10__delete_unused_tables.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS data.resource_permissions, data.resource_description, data.permission, data.permissions,
+    data.principal, data.resource, data.resources;
