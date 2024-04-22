c003c9cff5b8c9506c8b0d081b008f65427a4b45
Андрей Колосов <anilovasasa@gmail.com>
2024-04-22 16:41:04 +0300

    feat(data-service) update photo_uploader shema
    Related work items: #1802, #1835

--- data-service/src/main/resources/sql/schemas/photo_recording.sql
+++ data-service/src/main/resources/sql/schemas/photo_recording.sql
@@ -6,110 +6,118 @@ WHERE NOT EXISTS( SELECT id FROM data.schemas WHERE name = 'photo_uploader');
 UPDATE data.schemas 
 SET class_rule =
     '{
-        "tags": [
-            "system",
-            "Фотофиксация"
-        ],
-        "name": "photo_uploader",
-        "title": "Объекты фотофиксация",
-        "styleName": "photo_uploader",
-        "tableName": "photo_uploader",
-        "originName": "photo_uploader",
-        "properties": [
-            {
-                "name": "name",
-                "title": "Наименование объекта",
-                "required": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "sender",
-                "title": "Отправитель",
-                "readOnly": true,
-                "required": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "photographytime",
-                "title": "Дата и время съёмки",
-                "readOnly": true,
-                "required": true,
-                "valueType": "DATETIME"
-            },
-            {
-                "name": "creationtime",
-                "title": "Дата и время создания",
-                "readOnly": true,
-                "required": true,
-                "valueType": "DATETIME"
-            },
-            {
-                "name": "color",
-                "title": "цвет",
-                "hidden": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "color2",
-                "title": "цвет2",
-                "hidden": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "rotation",
-                "title": "Вращение",
-                "valueType": "STRING"
-            },
-            {
-                "name": "photo",
-                "title": "Фотофиксация",
-                "maxSize": 50000000,
-                "multiple": true,
-                "required": true,
-                "valueType": "FILE",
-                "maxDocuments": 20
-            },
-            {
-                "name": "last_modified",
-                "title": "Дата последнего изменения",
-                "hidden": true,
-                "valueType": "DATETIME"
-            },
-            {
-                "name": "created_at",
-                "title": "Дата создания",
-                "hidden": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "created_by",
-                "title": "Создатель",
-                "hidden": true,
-                "valueType": "STRING"
-            },
-            {
-                "name": "updated_by",
-                "title": "Кем изменено",
-                "hidden": true,
-                "valueType": "DATETIME"
-            },
-            {
-                "name": "shape",
-                "title": "Геометрия",
-                "hidden": true,
-                "valueType": "GEOMETRY",
-                "allowedValues": [
-                    "Point"
-                ]
-            },
-            {
-                "name": "ruleid",
-                "title": "Цифры",
-                "hidden": true,
-                "valueType": "STRING"
-            }
-        ],
-        "description": "Объекты для привязки фото к слою",
-        "geometryType": "Point"
-    }'
+  "tags": [
+    "system",
+    "Фотофиксация"
+  ],
+  "name": "photo_uploader",
+  "title": "Объекты фотофиксация",
+  "styleName": "photo_uploader",
+  "tableName": "photo_uploader",
+  "originName": "photo_uploader",
+  "properties": [
+    {
+      "name": "name",
+      "title": "Наименование объекта",
+      "required": true,
+      "valueType": "STRING"
+    },
+    {
+      "name": "sender",
+      "title": "Отправитель",
+      "readOnly": true,
+      "required": true,
+      "valueType": "USER_ID"
+    },
+    {
+      "name": "photography_time",
+      "title": "Дата и время съёмки",
+      "readOnly": true,
+      "required": true,
+      "valueType": "DATETIME"
+    },
+    {
+      "name": "creation_time",
+      "title": "Дата и время создания",
+      "readOnly": true,
+      "required": true,
+      "valueType": "DATETIME"
+    },
+    {
+      "name": "tags",
+      "title": "Теги",
+      "valueType": "STRING",
+      "maxLength": 100
+    },
+    {
+      "name": "user_fill_color",
+      "title": "цвет заливки по отправителю",
+      "hidden": true,
+      "calculatedValueFormula": "return (Number(''0x''+(Number(obj.sender)*71).toString(16) + (Number(obj.sender)*91).toString(12) + (Number(obj.sender)*51).toString(15) + (Number(obj.sender)*31).toString(14) + (Number(obj.sender)*11).toString(13))*95573/16).toString(16).slice(1, 7)",
+      "valueType": "STRING"
+    },
+    {
+      "name": "user_stroke_color",
+      "title": "цвет обводки по отправителю",
+      "hidden": true,
+      "calculatedValueFormula": "return (Number(''0x''+(Number(obj.sender)*71).toString(16) + (Number(obj.sender)*91).toString(12) + (Number(obj.sender)*51).toString(15) + (Number(obj.sender)*31).toString(14) + (Number(obj.sender)*11).toString(13))*73719/16).toString(16).slice(8, 14)",
+      "valueType": "STRING"
+    },
+    {
+      "name": "rotation",
+      "title": "Вращение",
+      "valueType": "STRING"
+    },
+    {
+      "name": "photo",
+      "title": "Фотофиксация",
+      "maxSize": 50000000,
+      "multiple": true,
+      "required": true,
+      "valueType": "FILE",
+      "maxDocuments": 20
+    },
+    {
+      "name": "last_modified",
+      "title": "Дата последнего изменения",
+      "hidden": true,
+      "valueType": "DATETIME"
+    },
+    {
+      "name": "created_at",
+      "title": "Дата создания",
+      "hidden": true,
+      "valueType": "STRING"
+    },
+    {
+      "name": "created_by",
+      "title": "Создатель",
+      "hidden": true,
+      "valueType": "STRING"
+    },
+    {
+      "name": "updated_by",
+      "title": "Кем изменено",
+      "hidden": true,
+      "valueType": "DATETIME"
+    },
+    {
+      "name": "shape",
+      "title": "Геометрия",
+      "hidden": true,
+      "valueType": "GEOMETRY",
+      "allowedValues": [
+        "Point"
+      ]
+    },
+    {
+      "name": "ruleid",
+      "title": "Цифры",
+      "hidden": true,
+      "valueType": "STRING"
+    }
+  ],
+  "description": "Объекты для привязки фото к слою",
+  "geometryType": "Point"
+}'
 WHERE name = 'photo_uploader';
