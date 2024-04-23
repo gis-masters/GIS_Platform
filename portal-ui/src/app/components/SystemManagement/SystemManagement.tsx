a43e2a3999d6de423b15c0e78363ba3f109987b6
Иван Николаенко < >
2024-04-23 10:38:24 +0300

    Merged PR 386: fix(ui: orgSettings) Fix Layout  of supe admin page
    Related work items: #1687, #1836

--- portal-ui/src/app/components/SystemManagement/SystemManagement.tsx
+++ portal-ui/src/app/components/SystemManagement/SystemManagement.tsx
@@ -4,6 +4,8 @@ import { cn } from '@bem-react/classname';
 import { SystemManagementHeader } from './Header/SystemManagement-Header';
 import { SystemManagementContent } from './Content/SystemManagement-Content';
 
+import '!style-loader!css-loader!sass-loader!./SystemManagement.scss';
+
 const cnSystemManagement = cn('SystemManagement');
 
 export const SystemManagement: FC = () => (
