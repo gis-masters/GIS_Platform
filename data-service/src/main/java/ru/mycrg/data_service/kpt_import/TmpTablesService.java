d707f7ce000f7ce44c7d693788261f8cd22496f0
Павел Туча <p.tucha@mycrg.ru>
2024-10-15 09:18:14 +0300

    Merged PR 622: refactoring(data-service) can not import name_by_doc field
    Перед отправкой PR, пожалуйста, удостоверьтесь, что следующие требования соблюдены:
    
    ##### Разработчик
    
    - [X] Функциональность эксплораторно или автоматически протестирована.
    ~~- [ ] Добавлены Acceptance Tests в виде Gherkin (Cucumber) Feature file (Given When Then).~~
    ~~- [ ] Acceptance Tests скопированы в WorkItem.~~
    - [X] Созданы Task-и на эксплораторное тестирование и они привязаны к PR.
    ~~- [ ] Создана PBI на обновление инструкции и она привязана к PR (и к PBI как Sucsessor).~~
    - [X] Итоговый вариант продемонстрирован продакт-овнеру
    
    ##### Менеджер проекта
    
    - [X] Все критерии приёмки отработаны исполнитеем.
    - [X] WorkItems (Bugs\PBI) привязаны к parent Feature в Backlog.
    ~~- [ ] Документ [Функции системы](https://1drv.ms/x/s!Amr-xlRBGCTvgmaq74bgGny-lMja) актуализирован.~~
    
    ##### Тестировщик
    
    - [X] Критерии приёмки протестированы.
    - [X] Все задачи на эксплораторное тестирование закрыты.
    
    Если какие-то пункты не применимы к задаче, то их следует вычеркнуть как в примере
    
    - [ ] ~~Так выглядит вычеркнутый пункт~~
    
    Related work items: #2547, #2597

--- data-service/src/main/java/ru/mycrg/data_service/kpt_import/TmpTablesService.java
+++ data-service/src/main/java/ru/mycrg/data_service/kpt_import/TmpTablesService.java
@@ -2,13 +2,17 @@ package ru.mycrg.data_service.kpt_import;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.mycrg.data_service.dao.config.DatasourceFactory;
+import ru.mycrg.data_service.dao.ddl.tables.DdlTablesBase;
 import ru.mycrg.data_service.dao.detached.DetachedRecordsDao;
-import ru.mycrg.data_service.dao.detached.KptImportDao;
 import ru.mycrg.data_service.dao.exceptions.CrgDaoException;
+import ru.mycrg.data_service.dao.utils.wellknown_formula_generator.PropertyBuilderWithFormula;
+import ru.mycrg.data_service.service.resources.ResourceQualifier;
 import ru.mycrg.data_service_contract.dto.SchemaDto;
 
+import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,13 +29,16 @@ public class TmpTablesService {
 
     private static final Logger log = LoggerFactory.getLogger(TmpTablesService.class);
 
-    private final KptImportDao kptImportDao;
     private final DetachedRecordsDao recordsDao;
+    private final DatasourceFactory datasourceFactory;
+    private final PropertyBuilderWithFormula propertyBuilderWithFormula;
 
-    public TmpTablesService(KptImportDao kptImportDao,
-                            DetachedRecordsDao recordsDao) {
-        this.kptImportDao = kptImportDao;
+    public TmpTablesService(DetachedRecordsDao recordsDao,
+                            DatasourceFactory datasourceFactory,
+                            PropertyBuilderWithFormula propertyBuilderWithFormula) {
         this.recordsDao = recordsDao;
+        this.datasourceFactory = datasourceFactory;
+        this.propertyBuilderWithFormula = propertyBuilderWithFormula;
     }
 
     /**
@@ -40,15 +47,19 @@ public class TmpTablesService {
      * Название таблицы = kpt_[Название схемы], состав полей в соответствии со схемой
      */
     public void recreateTable(String dbName, Collection<SchemaDto> schemas) {
-        for (SchemaDto schema: schemas) {
+
+        DataSource dataSource = datasourceFactory.getNamedDataSource(dbName, SYSTEM_SCHEMA_NAME);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        DdlTablesBase ddlTablesBase = new DdlTablesBase(jdbcTemplate, propertyBuilderWithFormula);
+
+        for (SchemaDto schema : schemas) {
             String tableName = tmbTableName(schema.getName());
 
             log.info("Удаление временной таблицы {}", tableName);
-            kptImportDao.dropTable(dbName, SYSTEM_SCHEMA_NAME, tableName);
+            ddlTablesBase.drop(new ResourceQualifier(SYSTEM_SCHEMA_NAME, tableName));
 
             log.info("Создание временной таблицы {}", tableName);
-            kptImportDao.createTable(dbName, SYSTEM_SCHEMA_NAME, tableName, schema.getProperties(),
-                    PRIMARY_KEY);
+            ddlTablesBase.create(SYSTEM_SCHEMA_NAME, tableName, schema.getProperties(), PRIMARY_KEY);
         }
     }
 
