c3928b61f5b15ad3acdac1f3094552f36206430d
Денис Алексеев <d.alekseev@mycrg.ru>
2025-10-23 10:15:37 +0300

    Merged PR 1011: refactor(data-service): remove SED
    Related work items: #3655

--- data-service/src/main/java/ru/mycrg/data_service/service/integrations/exceptions/IntegrationException.java
+++
@@ -1,8 +0,0 @@
-package ru.mycrg.data_service.service.integrations.exceptions;
-
-public class IntegrationException extends RuntimeException {
-
-    public IntegrationException(String message) {
-        super(message);
-    }
-}
